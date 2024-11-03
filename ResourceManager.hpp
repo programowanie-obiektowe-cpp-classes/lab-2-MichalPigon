82bc74d09e092cac8b4a5ad887869dcabc0d9fef
MichalPigon <michaal.pigon@gmail.com>
2024-11-03 21:37:42 +0100

    bedzieg

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -1,8 +1,30 @@
-#pragma once
+ #pragma once
 
 #include "Resource.hpp"
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+public:
+    ResourceManager() : resource_{new Resource{}} {}
+
+    ResourceManager(const ResourceManager& other): resource_{new Resource{*other.resource_}}{}
+
+    ResourceManager& operator=(const ResourceManager& other) {
+        if (this == &other) return *this;
+        delete resource_;
+        resource_ = new Resource{*other.resource_};
+        return *this;
+    }
+
+    ~ResourceManager() {
+        delete resource_;
+    }
+
+    double get() {
+        return resource_->get();
+    }
+
+private:
+    Resource* resource_;
+
 };
